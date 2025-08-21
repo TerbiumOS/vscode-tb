c7cfdb9689d2729e225deafcfdf9749368bfc64e
John <72810050+Notplayingallday383@users.noreply.github.com>
2025-08-21 13:16:52 -0400

    fix

--- package.sh
+++ package.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-zip vscode.app.zip $( find -not -path "./http.js" -not -path "./package.sh" -not -path "vscode.app.zip" -not -path "./node_modules/*" -not -path "./myDav/node_modules/*" -not -path "./.git/*" && find "./node_modules/vscode-web" )
+zip vscode.tapp.zip $( find -not -path "./http.js" -not -path "./package.sh" -not -path "vscode.tapp.zip" -not -path "./node_modules/*" -not -path "./myDav/node_modules/*" -not -path "./.git/*" && find "./node_modules/vscode-web" )
